e3dd5db0e7384f00667e4c08816442b2b4325f01
unknown <ayushsingh.8d@gmail.com>
2024-03-28 21:39:22 +0530

    fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button, Form, Input } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, Input, Spin } from 'antd';
 import axios from 'axios';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { Link, useNavigate } from 'react-router-dom';
@@ -8,7 +8,7 @@ import LOGO from '../assets/todos_logo.png';
 
 const Login = () => {
   const { getItem, setItem } = useLocalStorage('user');
-
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,6 +19,7 @@ const Login = () => {
 
   const onFinish = async (values) => {
     try {
+      setLoading(() => true);
       const { user_name, password } = values;
 
       const res = await axios.post(
@@ -31,7 +32,11 @@ const Login = () => {
       const { token } = res.data;
       setItem(token);
       window.location.reload();
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(() => false);
+    }
   };
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
@@ -90,7 +95,7 @@ const Login = () => {
           </Button>
         </Form.Item>
       </Form>
-
+      {loading && <Spin />}
       <div className="text-sm text-gray-400">
         Don't have an account ?{' '}
         <Link to="/register" className="text-gray-500">
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button, Form, Input } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, Input, Spin } from 'antd';
 import axios from 'axios';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { Link, useNavigate } from 'react-router-dom';
@@ -7,6 +7,8 @@ import { UserAddOutlined } from '@ant-design/icons';
 import LOGO from '../assets/todos_logo.png';
 
 const Register = () => {
+  const [loading, setLoading] = useState(false);
+
   const { getItem, setItem } = useLocalStorage('user');
   const navigate = useNavigate();
 
@@ -18,6 +20,8 @@ const Register = () => {
 
   const onFinish = async (values) => {
     try {
+      setLoading(() => true);
+
       const { user_name, password } = values;
 
       await axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/register`, {
@@ -37,6 +41,8 @@ const Register = () => {
       window.location.reload();
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(() => false);
     }
   };
   const onFinishFailed = (errorInfo) => {
@@ -96,7 +102,7 @@ const Register = () => {
           </Button>
         </Form.Item>
       </Form>
-
+      {loading && <Spin />}
       <div className="text-sm text-gray-400">
         Already registered ?{' '}
         <Link to="/login" className="text-gray-600">
